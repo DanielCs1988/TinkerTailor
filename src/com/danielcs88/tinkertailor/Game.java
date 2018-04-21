c6cdacf8438af4707571d94a4963ed2ecd65eabe
Emperor <daniel.cs20@gmail.com>
2018-04-21 19:14:26 +0200

    Game runs with normal ArrayList as well now.

--- src/com/danielcs88/tinkertailor/Game.java
+++ src/com/danielcs88/tinkertailor/Game.java
@@ -1,6 +1,6 @@
 package com.danielcs88.tinkertailor;
 
-import java.util.Iterator;
+import java.util.*;
 
 public final class Game {
 
@@ -16,14 +16,29 @@ public final class Game {
         Iterator<T> playerIterator = players.iterator();
         int rounds = players.size();
 
-        System.out.println(players);
-
         for (int i = 0; i < rounds - 1; i++) {
+            System.out.println(players);
             for (int j = 0; j < steps; j++) {
                 playerIterator.next();
             }
             playerIterator.remove();
+        }
+        return players.get(0);
+    }
+
+    public <T> T playTinkerTailor(List<T> players, int steps) {
+        int rounds = players.size();
+        Iterator<T> iter = players.iterator();
+
+        for (int i = 0; i < rounds - 1; i++) {
             System.out.println(players);
+            for (int j = 0; j < steps; j++) {
+                if (!iter.hasNext()) {
+                    iter = players.iterator();
+                }
+                iter.next();
+            }
+            iter.remove();
         }
         return players.get(0);
     }
--- src/com/danielcs88/tinkertailor/Main.java
+++ src/com/danielcs88/tinkertailor/Main.java
@@ -1,11 +1,17 @@
 package com.danielcs88.tinkertailor;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Game game = Game.getInstance();
+        runGameWithCustomList(game);
+        runGameWithArrayList(game);
+        runTests();
+    }
+
+    private static void runGameWithCustomList(Game game) {
         CircleList<String> players = new CircleList<>(Arrays.asList(
                 "Mythal (5)", "Andruil (4)", "Fen'Harel (3)", "Ghilan'nain (2)", "Dirthamen (1)"
         ));
@@ -17,8 +23,19 @@ public class Main {
         System.out.println("Every third being falls!\n");
 
         String result = game.playTinkerTailor(players, 3);
-        System.out.println("\nAnd the winner is: " + result);
+        System.out.println("\nAnd the winner is: " + result + "\n");
+    }
 
+    private static void runGameWithArrayList(Game game) {
+        List<String> players = new ArrayList<>(Arrays.asList(
+                "Dirthamen (1)", "Ghilan'nain (2)", "Fen'Harel (3)", "Andruil (4)",  "Mythal (5)"
+        ));
+        System.out.println("Now the same game with a basic ArrayList collection:\n");
+        String result = game.playTinkerTailor(players, 3);
+        System.out.println("\nNo surprise here, the result is the same: " + result);
+    }
+
+    private static void runTests() {
         System.out.println("\n<<<<< And now some normals tests with a different data type! >>>>>\n");
         Test test = Test.getInstance();
         test.start();
